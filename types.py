383774b12f457ebb517685a001e9c12f7146aa00
nramiscal <nramiscal@gmail.com>
2019-05-08 10:28:24 -0400

    first commit

---
+++ types.py
@@ -0,0 +1,26 @@
+a = [1, 2.0, "abacus"]
+b = {"name": "Example McExampleface", "email": "example@example.com"}
+c = (1, 2, 3)
+d = 1,2
+e = {"thing1": a, "thing2": b, "thing3": (a, b, c)}
+
+
+# print(type(e))
+# print(type(e["thing1"]))
+# print(type(e["thing1"][2]))
+# print(e["thing2"])
+# print(type(e["thing3"]))
+# print(type(e["thing3"][3]))
+print(type(e["thing3"][1]))
+
+# print(type(a))
+# print(a[1])
+# print(type(a[2]))
+# print(type(b))
+# print(type(b["name"]))
+# print(type(c))
+# print(type(d))
+# print(c)
+# c[0] = 100
+# print(c)
+# print(d)
